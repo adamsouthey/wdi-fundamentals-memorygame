25012b5bd96c6e26c01aabf45dd85db55348e850
Adam Southey <adamsouthey@Adams-MacBook-Pro.local>
2017-10-24 16:25:48 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,46 +1,40 @@
-/*
-console.log("User flipped " + cardOne + " card");
-console.log("User flipped " + cardThree + " card");
-*/
-
-
 
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped " + cardOne );
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardTwo );
-
-/*
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-console.log("User Flipped " + cardThree );
-
-var cardFour = cards[3];
-cardsInPlay.push(cardFour);
-console.log("User Flipped " + cardFour );
-
-*/
-
-
-
+var checkForMatch = function () {
 
 if (cardsInPlay.length === 2) {
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
+}	
+};
 
 
-/*var checkForMatch = function () {
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
 
 	
-}
-*/
+
+
+
+
+
+
+
+
+
